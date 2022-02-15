d9b4f63acf087090456f4fb08971b9b0f522f87b
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-02-15 12:05:57 -0500

    working on item form

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,5 +1,47 @@
 function RecipeForm(){
-    <h1>THIS IS THE RECIPE FORM</h1>
+    return(
+        <div>
+        <h1>Add a New Cocktail Recipe</h1>
+        <form>
+            <label>Cocktail Name:</label>
+            <br/>
+            <input type="text" name="name"></input>
+            <br/>
+            <label>Category:</label><br/>
+            <select name="catogory">
+                <option value="classic">Classic</option>
+                <option value="basic">Basic</option>
+                <option value="tiki">Tiki</option>
+            </select>
+            <br/>
+        </form>
+        <form name="ingrendients">
+                <label>Ingredient:</label>
+                <br/>
+                <input type="text" name="ingredient"></input>
+                <br/>
+                <label>Quantity:</label>
+                <br/>
+                <input type="text" name="quantity"></input>
+                <input type="submit" name="addIngredient" value="Add Ingredient"></input>
+        </form>
+        <form>
+        <label>Description:</label>
+            <br/>
+            <input type="text" name="description"></input>
+            <br/>
+            <label>Directions:</label>
+            <br/>
+            <input type="text" name="directions"></input>
+            <br/>
+            <label>Add Image (Optional):</label>
+            <br/>
+            <input type="text" name="Image"></input>
+            <br/>
+            <input type="submit" value="Submit Cocktail"></input>
+        </form>
+        </div>
+    )
 }
 
 export default RecipeForm;
