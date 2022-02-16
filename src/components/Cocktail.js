f32fe41dbab7fb1c1ab82c5af6a76e135e43e649
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-02-16 09:54:39 -0500

    first patch (favorite)

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,8 +1,28 @@
+// TODO IMPLEMENT PATCH ON FAVORITE
+
 import { useState } from 'react';
 
 function Cocktail ({ cocktail }){
     const [moreInfoDisplay, setMoreInfoDisplay] = useState(false);
-    const {name, image, ingredients, rating, category, directions, description, personalFavorite}=cocktail;
+    const { id, name, image, ingredients, personalFavorite, category, directions, description }=cocktail;
+    const [favorited, setFavorited]=useState(personalFavorite)
+
+    function handleFavorite(){
+        setFavorited(!favorited)
+        fetch(`http://localhost:3000/cocktails/${id}`, 
+        {
+            method: 'PATCH',
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(
+                {
+                    "personalFavorite": !favorited
+                }
+            ),
+          }
+          )
+    }
 
     const ingredientsList = ingredients.map(ingredient => <li key={ingredient.ingredient}>{ingredient.quantity} {ingredient.ingredient}</li>)
 
@@ -14,7 +34,7 @@ function Cocktail ({ cocktail }){
             <h3>{description}</h3>
             <p> {moreInfoDisplay ? ingredientsList : null } </p>
             <p> {moreInfoDisplay ? directions : null } </p>
-            <p>Rating: {rating}/5 Stars</p>
+            <button className="favorite" onClick={handleFavorite}>{favorited ? "Unfavorite ★" : "Favorite ✰"}</button>
         </div>
     )
 
